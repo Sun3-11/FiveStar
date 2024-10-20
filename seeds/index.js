dc74e546b834e5d2f33fe6e4894807b0864c0308
SunAltahri <sunshine.arts.3@gmail.com>
2024-10-20 21:55:24 +0300

    edit

--- seeds/index.js
+++ seeds/index.js
@@ -1,67 +1,70 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const{places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Placeground = require('../models/placeground');
 
-mongoose.set('strictQuery', true); 
-mongoose.connect('mongodb://localhost:27017/yemen-review', {
- 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.set('strictQuery', true);
 
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/yemen-review', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Connection error:", error);
+        process.exit(1);
+    }
+}
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Placeground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 5);
+    try {
+        await Placeground.deleteMany({}); 
         
-        const price = Math.floor(Math.random() * 2) + 1;
-        const place = new Placeground({
-            // USER ID
-            author: "63d58efe6e596f1c5c22c5f9",
-            location: `${cities[random1000].city}, ${cities[random1000].admin_name}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-           // image: 'https://source.unsplash.com/483251', 
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quo praesentium in ratione odio recusandae. Mollitia suscipit, beatae accusamus ad eos sequi',
-            price,
-            //typeplace : 'fruit',//`${typesplases[random1000].typess}`,           
-            geometry: {
-                type: "Point",
-                coordinates: [
-                     // coordinates: [15.240570, 44.657327] 
-                    cities[random1000].lng,
-                    cities[random1000].lat,
+        for (let i = 0; i < 50; i++) {
+            const random1000 = Math.floor(Math.random() * cities.length); 
+            const price = Math.floor(Math.random() * 2) + 1;
+
+            const place = new Placeground({
+                // USER ID
+                author: "63d58efe6e596f1c5c22c5f9",
+                location: `${cities[random1000].city}, ${cities[random1000].admin_name}`,
+                title: `${sample(descriptors)} ${sample(places)}`,
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quo praesentium in ratione odio recusandae. Mollitia suscipit, beatae accusamus ad eos sequi',
+                price,
+                geometry: {
+                    type: "Point",
+                    coordinates: [
+                        cities[random1000].lng,
+                        cities[random1000].lat
+                    ]
+                },
+                image: [
+                    {
+                        url: '/img/bg1.jpg',
+                        filename: 'yemenReviews/l4lvvehb4jdbiqqoogkz',
+                    },
+                    {
+                        url: '/img/bg5.jpg',
+                        filename: 'yemenReviews/aiddnomq8yk25khh5klb',
+                    },
+                    {
+                        url: '/img/ig4.jpg',
+                        filename: 'yemenReviews/l4lvvehb4jdbiqqoogkz',
+                    }
                 ]
-            },
-            image: [
-                {
-                    url: 'https://res.cloudinary.com/db8dzgriv/image/upload/v1675642244/yemenReviews/l4lvvehb4jdbiqqoogkz.jpg',
-                    filename: 'yemenReviews/l4lvvehb4jdbiqqoogkz',
-                    
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/db8dzgriv/image/upload/v1675642254/yemenReviews/aiddnomq8yk25khh5klb.jpg',
-                    filename: 'yemenReviews/aiddnomq8yk25khh5klb',
-                    
-                  },
-                
-            ]
-            
-        })
-        await place.save();
+            });
+            await place.save();
+        }
+    } catch (error) {
+        console.error("Error seeding database:", error);
     }
-}
-seedDB();
-seedDB().then(() => {
-    mongoose.connection.close();
-    
-})
+};
+
+connectDB().then(async () => {
+    await seedDB();
+    mongoose.connection.close(); 
+});
